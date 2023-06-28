eda00252f10d95aa543645b2515c28ad80d72cd5
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-27 22:14:30 -0500

    creando el controlador para el registro de usuarios y almacenamiento en la base de datos

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,37 @@
-export const registro = (req, res) => {
-    res.send('Registro de Usuarios')
-}
+import Usuario from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { creatAccesoToken } from "../libs/jwt.js";
+
+export const registro = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const passwordHash = await bcryptjs.hash(password, 10);
+
+    const nuevoUsuario = new Usuario({
+      username,
+      email,
+      password: passwordHash,
+    });
+
+    const usuarioGuardado = await nuevoUsuario.save();
+    /* llamamos a la función creatAccesoToken definida en el directorio libs */
+    const token = await creatAccesoToken({ id: usuarioGuardado._id });
+    res.cookie("token", token);
+
+    /* aquí se definen solo los atributos que se van a utilizar en el frontend */
+    res.json({
+      id: usuarioGuardado._id,
+      username: usuarioGuardado.username,
+      email: usuarioGuardado.email,
+      createdAt: usuarioGuardado.createdAt,
+      updatedAt: usuarioGuardado.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 export const inicio_sesion = (req, res) => {
-    res.send('Inicio de Sesión')
-}
+  res.send("Inicio de Sesión");
+};
