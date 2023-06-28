59f971965e40fcc39d8e8a3f47762f114d71dbd7
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-27 22:12:18 -0500

    creando el middlewaere para la utilización de json dentro del proyecto

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
-import express from 'express'
-import morgan from 'morgan'
+import express from 'express';
+import morgan from 'morgan';
 
-const app = express()
+import authRutas from './routes/auth.routes.js'
 
-app.use(morgan('dev'))
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api',authRutas);
 
 export default app;     
