f3448aa212bcbc8033a43bf75785b3b68ab772b3
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-27 22:13:10 -0500

    determinando el modelo para usuarios con sus respectivos atributos

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const UserSchema = mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true
@@ -15,6 +15,8 @@ const UserSchema = mongoose.Schema({
         type: String,
         required: true
     }
+},{
+    timestamps: true
 });
 
 export default mongoose.model('User', UserSchema);
