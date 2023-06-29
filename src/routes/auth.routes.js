1be1fc644c2097587f30277ece4516f7b68251b2
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-28 21:42:32 -0500

    creando las rutas para cerrar_sesion y para el incio del sistema

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,13 @@
 import {Router} from 'express';
-import {registro, inicio_sesion} from '../controllers/auth.controller.js'
+import {registro, iniciar_sesion, cerrar_sesion, inicio} from '../controllers/auth.controller.js'
+import {autenticacionRequerida} from '../middlewares/validateToken.js'
 
 const router = Router();
 
 router.post('/registro', registro);
-router.post('/inicio_sesion', inicio_sesion);
+router.post('/iniciar_sesion', iniciar_sesion);
+router.post('/cerrar_sesion', cerrar_sesion);
+
+router.get('/inicio', autenticacionRequerida, inicio)
 
 export default router;
