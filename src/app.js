729c1e876083838d6b064d1caccc17f0d9cc7d30
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-30 18:46:51 -0500

    añadiendo en el servidor la ruta principal para los bienes raices

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser'
 
 import authRutas from './routes/auth.routes.js'
+import bienesRutas from './routes/realty.routes.js'
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api',authRutas);
+app.use('/api/',bienesRutas);
 
 export default app;     
