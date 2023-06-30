ad674fd368f1d2bf4492f946348dd916d354dd30
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-29 22:23:01 -0500

    creando el controlador para los bienes raices en donde se definen todas las funciones del crud para las bienes raices

---
+++ src/controllers/realty.controller.js
@@ -0,0 +1,21 @@
+import BienesRaices from '../models/realty.model.js';
+
+export const listaBienesRaices = async (req, res) =>{
+
+};
+
+export const crearBienesRaices = async (req, res) =>{
+    
+};
+
+export const obtenerBienesRaices = async (req, res) =>{
+    
+};
+
+export const actualizarBienesRaices = async (req, res) =>{
+    
+};
+
+export const eliminarBienesRaices = async (req, res) =>{
+    
+};
