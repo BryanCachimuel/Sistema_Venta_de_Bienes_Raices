53cb101efdd6a15225f5db3de0575d743cd35c27
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-30 21:48:34 -0500

    realizando la validación de datos para el inicio de sesión de un usuario

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -4,20 +4,31 @@ import {z} from 'zod';
 
 export const RegistroEsquema = z.object({
     username: z.string({
-        required_error: 'Nombre de Usuario Requerido'
+        required_error: "Nombre de Usuario Requerido",
     }),
     email: z.string({
-        required_error: 'Correo de Usuario Requerido'
+        required_error: "Correo de Usuario Requerido",
     }).email({
-        message: 'Correo Invalido'
+        message: "Correo Invalido",
     }),
     password: z.string({
-        required_error: 'La Contraseña es Requerida'
+        required_error: "La Contraseña es Requerida",
     }).min(6, {
-        message: 'La Contraseña debe tener más de 6 caracteres'
+        message: "La Contraseña debe tener mínimo 6 caracteres",
     }),
 });
 
-
+export const InicioSesionEsquema = z.object({
+    email: z.string({
+        required_error: "Correo de Usuario Requerido",
+    }).email({
+        message: "Correo Invalido",
+    }),
+    password: z.string({
+        required_error: "La Contraseña es Requerida",
+    }).min(6, {
+        message: "La Contraseña debe tener mínimo 6 caracteres",
+    }),
+});
 
 
