b6ea679d730534f3d93a7cfa2a7609193e63daae
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-30 19:37:32 -0500

    creando las funciones para actualizarBienRaiz y eliminarBienRaiz dentro del controller de Bienes Raices

--- src/controllers/realty.controller.js
+++ src/controllers/realty.controller.js
@@ -37,3 +37,23 @@ export const obtenerBieneRaiz = async (req, res) =>{
     }
 };
 
+export const actualizarBieneRaiz = async (req, res) =>{
+    try {
+        /* el 3er parámetro {new:true} hace referencia para obtener el dato nuevo que se acaba de actualizar */
+        const actualizaBienRaiz = await BienesRaices.findByIdAndUpdate(req.params.id, req.body, {new:true});
+        if(!actualizaBienRaiz) return res.status(404).json({ message: "Bien Raíz no Encontrado" });
+        res.json(actualizaBienRaiz);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const eliminarBienRaiz = async (req, res) =>{
+    try {
+        const borrarBienRaiz = await BienesRaices.findByIdAndDelete(req.params.id);
+        if(!borrarBienRaiz) return res.status(404).json({ message: "Bien Raíz no Encontrado" });
+        res.json(borrarBienRaiz);   
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
