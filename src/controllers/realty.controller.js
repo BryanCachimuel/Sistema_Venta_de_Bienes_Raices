744c46e6952147c1e7aeeec5f262b8cdf1e01b15
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-30 19:36:18 -0500

    creando las funciones para listarBienesRaices, crearBienRaiz y obtenerBienRaiz dentro del controller de Bienes Raices

--- src/controllers/realty.controller.js
+++ src/controllers/realty.controller.js
@@ -1,21 +1,39 @@
 import BienesRaices from '../models/realty.model.js';
 
 export const listaBienesRaices = async (req, res) =>{
-
+    try {
+        const bienesRaices = await BienesRaices.find();
+        res.json(bienesRaices); 
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 };
 
-export const crearBienesRaices = async (req, res) =>{
-    
+export const crearBienRaiz = async (req, res) =>{
+    const { inmueble, descripcion, propietario, costo, antiguedad, fecha } = req.body;
+    try {
+        const bienraiz = new BienesRaices({
+            inmueble,
+            descripcion,
+            propietario,
+            costo,
+            antiguedad,
+            fecha
+        });
+        const bienraizGuardado = await bienraiz.save();
+        res.json(bienraizGuardado);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
-export const obtenerBienesRaices = async (req, res) =>{
-    
+export const obtenerBieneRaiz = async (req, res) =>{
+    try {
+       const bienraiz = await BienesRaices.findById(req.params.id);
+       if(!bienraiz) return res.status(404).json({ message: "Bien Raíz no Encontrado" }); 
+       res.json(bienraiz);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
-export const actualizarBienesRaices = async (req, res) =>{
-    
-};
-
-export const eliminarBienesRaices = async (req, res) =>{
-    
-};
