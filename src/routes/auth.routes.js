6c08feab931f64ef96534ef6f4e3b7517b6c5daf
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-30 22:18:28 -0500

    importando los archivos necesarios para poder hacer las validaciones de los datos que se ingresen tanto al registrar como al iniciar sesión de usuarios

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,15 @@
 import {Router} from 'express';
-import {registro, iniciar_sesion, cerrar_sesion, inicio} from '../controllers/auth.controller.js'
-import {autenticacionRequerida} from '../middlewares/validateToken.js'
+import {registro, iniciar_sesion, cerrar_sesion, inicio} from '../controllers/auth.controller.js';
+import {autenticacionRequerida} from '../middlewares/validateToken.js';
+
+/* realizando las validaciones para los esquemas de registro e inicio de sesión */
+import {validarEsquema} from '../middlewares/validator.middleware.js';
+import {RegistroEsquema, InicioSesionEsquema} from '../schemas/auth.schema.js'
 
 const router = Router();
 
-router.post('/registro', registro);
-router.post('/iniciar_sesion', iniciar_sesion);
+router.post('/registro', validarEsquema(RegistroEsquema), registro);
+router.post('/iniciar_sesion', validarEsquema(InicioSesionEsquema), iniciar_sesion);
 router.post('/cerrar_sesion', cerrar_sesion);
 
 router.get('/inicio', autenticacionRequerida, inicio)
