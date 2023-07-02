664486bab4b2798c8d3a6d9cb7cea335ddacf40a
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-01 19:54:45 -0500

    agregando los objetos de validación de atributos hacia las rutas pertenecientes a crear y actualizar bienes raices

--- src/routes/realty.routes.js
+++ src/routes/realty.routes.js
@@ -1,13 +1,16 @@
 import {Router} from 'express';
 import {autenticacionRequerida} from '../middlewares/validateToken.js';
-import {listaBienesRaices, crearBienesRaices, obtenerBienesRaices, actualizarBienesRaices, eliminarBienesRaices} from '../controllers/realty.controller.js'
+import {listaBienesRaices, crearBienRaiz, obtenerBieneRaiz, eliminarBienRaiz, actualizarBieneRaiz} from '../controllers/realty.controller.js';
+
+import {validarEsquema} from '../middlewares/validator.middleware.js';
+import {crearBienRaizSchema, actualizarBienRaizSchema} from '../schemas/realty.schema.js'
 
 const router = Router();
 
 router.get('/bienes_raices', autenticacionRequerida, listaBienesRaices);
-router.get('/bienes_raices/:id', autenticacionRequerida, obtenerBienesRaices);
-router.post('/bienes_raices', autenticacionRequerida, crearBienesRaices);
-router.delete('/bienes_raices/:id', autenticacionRequerida, eliminarBienesRaices);
-router.put('/bienes_raices/:id', autenticacionRequerida, actualizarBienesRaices);
+router.get('/bienes_raices/:id', autenticacionRequerida, obtenerBieneRaiz);
+router.post('/bienes_raices', autenticacionRequerida, validarEsquema(crearBienRaizSchema), crearBienRaiz);
+router.delete('/bienes_raices/:id', autenticacionRequerida, eliminarBienRaiz);
+router.put('/bienes_raices/:id', autenticacionRequerida, validarEsquema(actualizarBienRaizSchema), actualizarBieneRaiz);
 
 export default router;
