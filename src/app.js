e0418162cba4bb1d468c265c2ecd9ffdfcc8265c
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-03 20:14:02 -0500

    configurando en el backend la dependencia cors para el correcto funcionamiento ´de envio de información entre frontend y backend

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
-import authRutas from './routes/auth.routes.js'
-import bienesRutas from './routes/realty.routes.js'
+import authRutas from './routes/auth.routes.js';
+import bienesRutas from './routes/realty.routes.js';
 
 const app = express();
 
+/* le decimos mediante este cors que el backend funcione solo con el frontend a traves del puerto 5173 */
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
