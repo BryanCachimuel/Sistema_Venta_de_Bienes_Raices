6dc36f0ad4f775cf7013ccb8f6f266baa23273a1
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-04 20:43:44 -0500

    importando el estado de errores desde el archivo AuthContext y definiendo un recuadró encima del formulario de registro para que informe cuando se están cometiendo errores de ingreso de información de los nuevos usuarios

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -5,8 +5,8 @@ import { useEffect } from "react";
 import {useNavigate} from 'react-router-dom'
 
 function RegisterPage() {
-  const { register, handleSubmit } = useForm();
-  const {registroUsuarios, isAuthenticated} = useAuth();
+  const { register, handleSubmit, formState:{errors} } = useForm();
+  const {registroUsuarios, isAuthenticated, errors: registroErrores} = useAuth();
   const navegacion = useNavigate();
   
   useEffect(() => {
@@ -19,6 +19,13 @@ function RegisterPage() {
 
   return (
     <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+      {
+        registroErrores.map((error, i) => (
+          <div className="bg-red-500 p-2 text-white" key={i}>
+            {error}
+          </div>
+        ))
+      }
       <form onSubmit={registroOnSubmit}>
         <input
           type="text"
@@ -26,19 +33,21 @@ function RegisterPage() {
           {...register("username", { required: true })}
           placeholder="Nombre de Usuario"
         />
-
+        {errors.username && (<p className="text-red-500">Nombre de Usuario Requerido</p>)}
         <input
           type="email"
           className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
           {...register("email", { required: true })}
           placeholder="Correo Electrónico"
         />
+        {errors.email && (<p className="text-red-500">El Correo de Usuario Requerido</p>)}
         <input
           type="password"
           className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
           {...register("password", { required: true })}
           placeholder="Contraseña"
         />
+        {errors.password && (<p className="text-red-500">Contraseña de Usuario Requerido</p>)}
         <button type="submit">Registrar</button>
       </form>
     </div>
