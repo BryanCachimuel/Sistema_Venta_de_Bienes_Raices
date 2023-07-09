5a342a142200c7436799157498d971b93adf0e88
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-08 21:04:21 -0500

    consumiendo mediante axios el endpoint de iniciar_sesión creado en el servidor del sistema

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -4,3 +4,4 @@ const API = 'http://localhost:3005/api';
 
 export const registroRequest = user => axios.post(`${API}/registro`, user);
 
+export const inicioSesionRequest = user => axios.post(`${API}/iniciar_sesion`, user);
