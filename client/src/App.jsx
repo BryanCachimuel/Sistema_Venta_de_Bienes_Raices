a314e840f5fbf632b980537ab442cafec3aacf5f
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-08 22:02:21 -0500

    adecuando los links para las rutas que se necesitará para el sistema

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Pagina Inicio</h1>} />
+          <Route path="/" element={<h1>Inicio</h1>} />
           <Route path="/iniciar_sesion" element={<Inicio_Sesion />} />
           <Route path="/registro" element={<Registro />} />
           <Route path="/bienes_raices" element={<h1>Bienes Raices</h1>} />
