1cb4c5b34c40987290a063c230b71036005b5ec7
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-09 22:26:55 -0500

    desarrollando un script para validar el inicio de sesión de los usuarios y con ello proteger las rutas necesarias del sistema

---
+++ client/src/ProtectedRoute.jsx
@@ -0,0 +1,21 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+import { useAuth } from "./context/AuthContext";
+
+function ProtectedRoute() {
+  const { user, isAuthenticated } = useAuth();
+
+  {
+    /* verificando si el usuario esta autenticado.
+        se le pone replace para que no vuelva a  la ruta anterior.
+    */
+  }
+  if (!isAuthenticated) return <Navigate to={"/iniciar_sesion"} replace />;
+
+  {
+    /* si el usuario si esta logeado con el Outlet le dice que continue con la ruta siguiente que está autorizado por que si a podido iniciar sesión */
+  }
+  return <Outlet />;
+}
+
+export default ProtectedRoute;
