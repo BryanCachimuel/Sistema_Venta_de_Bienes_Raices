d2967dd0e9d0677dce1652f91e52f5fb7280d3fa
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-09 20:09:02 -0500

    añadiendo estilos a los botones y insertando un link en caso de que ya haya registrado se proceda a iniciar sesión

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -2,7 +2,7 @@
 import { useForm } from "react-hook-form";
 import {useAuth} from '../context/AuthContext';
 import { useEffect } from "react";
-import {useNavigate} from 'react-router-dom'
+import {useNavigate, Link} from 'react-router-dom'
 
 function RegisterPage() {
   const { register, handleSubmit, formState:{errors} } = useForm();
@@ -10,7 +10,7 @@ function RegisterPage() {
   const navegacion = useNavigate();
   
   useEffect(() => {
-    if(isAuthenticated) navegacion("/bienes_raices");
+    if(isAuthenticated) navegacion("/iniciar_sesion");
   }, [isAuthenticated])
 
   const registroOnSubmit = handleSubmit(async (values) => {
@@ -18,38 +18,45 @@ function RegisterPage() {
   });
 
   return (
-    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
-      {
-        registroErrores.map((error, i) => (
-          <div className="bg-red-500 p-2 text-white" key={i}>
-            {error}
-          </div>
-        ))
-      }
-      <form onSubmit={registroOnSubmit} autoComplete="off">
-        <input
-          type="text"
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-          {...register("username", { required: true })}
-          placeholder="Nombre de Usuario"
-        />
-        {errors.username && (<p className="text-red-500">Nombre de Usuario Requerido</p>)}
-        <input
-          type="email"
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-          {...register("email", { required: true })}
-          placeholder="Correo Electrónico"
-        />
-        {errors.email && (<p className="text-red-500">El Correo de Usuario Requerido</p>)}
-        <input
-          type="password"
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-          {...register("password", { required: true })}
-          placeholder="Contraseña"
-        />
-        {errors.password && (<p className="text-red-500">Contraseña de Usuario Requerido</p>)}
-        <button type="submit">Registrar</button>
-      </form>
+    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+      <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+        {
+          registroErrores.map((error, i) => (
+            <div className="bg-red-500 p-2 text-white" key={i}>
+              {error}
+            </div>
+          ))
+        }
+         <h1 className="text-2xl font-bold mb-3">Registro de Usuarios</h1>
+        <form onSubmit={registroOnSubmit} autoComplete="off">
+          <input
+            type="text"
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+            {...register("username", { required: true })}
+            placeholder="Nombre de Usuario"
+          />
+          {errors.username && (<p className="text-red-500">Nombre de Usuario Requerido</p>)}
+          <input
+            type="email"
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+            {...register("email", { required: true })}
+            placeholder="Correo Electrónico"
+          />
+          {errors.email && (<p className="text-red-500">El Correo de Usuario Requerido</p>)}
+          <input
+            type="password"
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+            {...register("password", { required: true })}
+            placeholder="Contraseña"
+          />
+          {errors.password && (<p className="text-red-500">Contraseña de Usuario Requerido</p>)}
+          <button className="className=bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-4 border border-white hover:border-transparent rounded mb-6 mt-3" type="submit">Registrar</button>
+        </form>
+        <hr />
+        <p className="flex gap-x-2 justify-between mt-3">
+            Si ya sea registrado por favor, <Link to="/iniciar_sesion" className="text-sky-500">Inicie Sesión</Link>
+          </p>
+      </div>
     </div>
   );
 }
