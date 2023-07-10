9be66407c2ad576a9d0b2089cdbd5b7ba015709b
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-09 22:29:09 -0500

    Establiciendo una ruta que engloba a las rutas protegidas en caso de inicio de sesión de usuarios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,29 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Registro from "./pages/RegisterPage";
 import Inicio_Sesion from "./pages/LoginPage";
+import RealtyPage from "./pages/RealtyPage";
+import RealtyFormPage from "./pages/RealtyFormPage";
+import ProfilePage from "./pages/ProfilePage";
+import HomePage from "./pages/HomePage";
+
+import ProtectedRoute from './ProtectedRoute'
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Inicio</h1>} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/iniciar_sesion" element={<Inicio_Sesion />} />
           <Route path="/registro" element={<Registro />} />
-          <Route path="/bienes_raices" element={<h1>Bienes Raices</h1>} />
-          <Route path="/crear_bienes_raices" element={<h1>Crear Bienes Raices</h1>}/>
-          <Route path="/bienes_raices/:id" element={<h1>Actualizar Bienes Raices</h1>}/>
-          <Route path="/perfil_usuario" element={<h1>Perfil de Usuarios</h1>} />
+
+          {/* Rutas protegidas */}
+          <Route element={<ProtectedRoute/>}>
+            <Route path="/bienes_raices" element={<RealtyPage />} />
+            <Route path="/crear_bienes_raices" element={<RealtyFormPage />} />
+            <Route path="/bienes_raices/:id" element={<RealtyFormPage />} />
+            <Route path="/perfil_usuario" element={<ProfilePage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
