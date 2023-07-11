4e1ca9524c882963687ea51446349b83368c790b
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-10 20:46:01 -0500

    creando la ruta verificar donde se define la función verificarToken

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import {registro, iniciar_sesion, cerrar_sesion, inicio} from '../controllers/auth.controller.js';
+import {registro, iniciar_sesion, cerrar_sesion, inicio, verificarToken} from '../controllers/auth.controller.js';
 import {autenticacionRequerida} from '../middlewares/validateToken.js';
 
 /* realizando las validaciones para los esquemas de registro e inicio de sesión */
@@ -12,6 +12,8 @@ router.post('/registro', validarEsquema(RegistroEsquema), registro);
 router.post('/iniciar_sesion', validarEsquema(InicioSesionEsquema), iniciar_sesion);
 router.post('/cerrar_sesion', cerrar_sesion);
 
-router.get('/inicio', autenticacionRequerida, inicio)
+router.get('/verificar', verificarToken);
+
+router.get('/perfil_usuario', autenticacionRequerida, inicio)
 
 export default router;
