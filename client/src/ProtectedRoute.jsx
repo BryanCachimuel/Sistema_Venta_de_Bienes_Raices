0b4efd0c13f7df6227f5cfe9b301d44a6f015d4c
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-10 21:43:00 -0500

    Validando mediante un loading y autenticación el proceso de protección de rutas

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -3,14 +3,16 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 
 function ProtectedRoute() {
-  const { user, isAuthenticated } = useAuth();
-
+  const { loading, isAuthenticated } = useAuth();
+  console.log(loading, isAuthenticated)
   {
     /* verificando si el usuario esta autenticado.
         se le pone replace para que no vuelva a  la ruta anterior.
     */
   }
-  if (!isAuthenticated) return <Navigate to={"/iniciar_sesion"} replace />;
+
+  if(loading) return <h1>Cargando...</h1>
+  if (!loading && !isAuthenticated) return <Navigate to={"/iniciar_sesion"} replace />;
 
   {
     /* si el usuario si esta logeado con el Outlet le dice que continue con la ruta siguiente que está autorizado por que si a podido iniciar sesión */
