98eed16d05c02f7c4bcb0ff9465c81b64c318681
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-22 20:23:41 -0500

    obteniendo la función obtenerTodosBienesRaices y renderizando todos los documentos registrados de bienes raices por un usuario

--- client/src/pages/RealtyPage.jsx
+++ client/src/pages/RealtyPage.jsx
@@ -1,6 +1,26 @@
+import { useEffect } from 'react';
+import {useRealty} from '../context/RealtyContext'
+
 function RealtyPage() {
+
+  const {obtenerTodosBienesRaices, realtys} = useRealty();
+  
+  useEffect(() => {
+    obtenerTodosBienesRaices();
+  },[])
+
   return (
-    <div>RealtyPage</div>
+    <div>
+      {realtys.map((realty) =>(
+        <div key={realty._id}>
+          <h1>{realty.inmueble}</h1>
+          <p>{realty.descripcion}</p>
+          <p>{realty.propietario}</p>
+          <p>{realty.costo}</p>
+          <p>{realty.antiguedad}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
