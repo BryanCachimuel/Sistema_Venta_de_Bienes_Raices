c0fc77da3d3cf8ba52e520cb217697b4dbd0fc6e
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-25 22:14:42 -0500

    actualizando la parte de la navegación del sistema

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,31 +7,34 @@ import RealtyFormPage from "./pages/RealtyFormPage";
 import ProfilePage from "./pages/ProfilePage";
 import HomePage from "./pages/HomePage";
 
-import ProtectedRoute from './ProtectedRoute'
+import ProtectedRoute from "./ProtectedRoute";
 import { RealtyProvider } from "./context/RealtyContext";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <AuthProvider>
-     <RealtyProvider>
-      <BrowserRouter>
-      <Navbar/>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/iniciar_sesion" element={<Inicio_Sesion />} />
-            <Route path="/registro" element={<Registro />} />
+      <RealtyProvider>
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/iniciar_sesion" element={<Inicio_Sesion />} />
+              <Route path="/registro" element={<Registro />} />
 
-            {/* Rutas protegidas */}
-            <Route element={<ProtectedRoute/>}>
-              <Route path="/bienes_raices" element={<RealtyPage />} />
-              <Route path="/crear_bienes_raices" element={<RealtyFormPage />} />
-              <Route path="/bienes_raices/:id" element={<RealtyFormPage />} />
-              <Route path="/perfil_usuario" element={<ProfilePage />} />
-            </Route>
-          </Routes>
+              {/* Rutas protegidas */}
+              <Route element={<ProtectedRoute />}>
+                <Route path="/bienes_raices" element={<RealtyPage />} />
+                <Route
+                  path="/crear_bienes_raices"
+                  element={<RealtyFormPage />}
+                />
+                <Route path="/bienes_raices/:id" element={<RealtyFormPage />} />
+                <Route path="/perfil_usuario" element={<ProfilePage />} />
+              </Route>
+            </Routes>
         </BrowserRouter>
-     </RealtyProvider>
+      </RealtyProvider>
     </AuthProvider>
   );
 }
