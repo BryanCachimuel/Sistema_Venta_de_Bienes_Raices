407f90d302b50a1951ccd731589e43d72dcfd46c
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-26 20:45:51 -0500

    dando formato a la fecha que se ve en las tarjetas de presentación de cada bien raíz registrado

--- client/src/components/RealtyCard.jsx
+++ client/src/components/RealtyCard.jsx
@@ -1,5 +1,10 @@
 import { useRealty } from "../context/RealtyContext";
 import {Link} from 'react-router-dom';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+
+/* para hacer la transformación exacata de la fecha */
+dayjs.extend(utc);
 
 function RealtyCard({realty}) {
 
@@ -10,15 +15,25 @@ function RealtyCard({realty}) {
       <header className="flex justify-between">
         <h1 className="text-2xl font-bold">{realty.inmueble}</h1>
         <div className="flex gap-x-2 items-center">
-          <button onClick={() =>{eliminarBienRaiz(realty._id)}}>Eliminar</button>
-          <Link to={`/bienes_raices/${realty._id}`}>Editar</Link>
+          <button 
+            onClick={() =>{eliminarBienRaiz(realty._id)}}
+            className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md"
+          >
+            Eliminar
+          </button>
+          <Link 
+            to={`/bienes_raices/${realty._id}`}
+            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md"
+          >
+            Editar
+          </Link>
         </div>
       </header>
       <p className="text-slate-300">{realty.descripcion}</p>
       <p className="text-slate-300">{realty.propietario}</p>
       <p className="text-slate-300">{realty.costo}</p>
       <p className="text-slate-300">{realty.antiguedad}</p>
-      <p className="text-slate-300">{new Date(realty.fecha).toLocaleDateString()}</p>
+      <p className="text-slate-300">{dayjs(realty.fecha).utc().format("DD/MM/YYYY")}</p>
     </div>
   );
 }
